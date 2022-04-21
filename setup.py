cb0d472f2220855234fd56e30f1099d2d06e9faf
Haimasree <haimasree.de@gmail.com>
2022-04-21 19:31:59 +0200

    Bump version: 0.7.5 → 0.7.6

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 desc = "kipoi-utils: utils used in various packages related to kipoi"
 setup(
     name='kipoi_utils',
-    version='0.7.5',
+    version='0.7.6',
     description=desc,
     author="Kipoi team",
     author_email='thorsten.beier@embl.de',
